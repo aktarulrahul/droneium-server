cd97cbdb9431f5c7311015425b8ea2ab5bf5f8be
aktarulrahul <contact.aktarul@gmail.com>
2021-11-10 19:20:37 +0600

    setup all users and orders apis

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function mongodbCURD() {
     ------------------------------------- */
     const database = client.db('droneium');
     const droneCollection = database.collection('drones');
+    const userCollection = database.collection('users');
     const orderCollection = database.collection('orders');
     const reviewCollection = database.collection('reviews');
     /* ------------------------------------- 
@@ -66,6 +67,86 @@ async function mongodbCURD() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await droneCollection.deleteOne(query);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    Make An Admin API
+    ------------------------------------- */
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (result?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+    /* ------------------------------------- 
+    Add a New User API
+    ------------------------------------- */
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    Upsert User API
+    ------------------------------------- */
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(query, updateDoc, options);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    GET All order API
+    ------------------------------------- */
+    app.get('/orders', async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    /* ------------------------------------- 
+    GET Single order API
+    ------------------------------------- */
+    app.get('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollection.findOne(query);
+      res.json(order);
+    });
+    /* ------------------------------------- 
+    POST Single order API
+    ------------------------------------- */
+    app.post('/orders', async (req, res) => {
+      // Step 1. data
+      const orderInfo = req.body;
+      // Step 2. insertOne
+      const result = await orderCollection.insertOne(orderInfo);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    DELETE Single order API
+    ------------------------------------- */
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    Update Single order Status API
+    ------------------------------------- */
+    app.put('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedStatus = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.updateOne(query, {
+        $set: { status: updatedStatus },
+      });
       res.json(result);
     });
   } finally {
